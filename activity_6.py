d4dda2d2e07a1770c00079eb65f8877ebe2268a2
Cyber Range Student <student@ip-100-64-53-8.ec2.internal>
2023-09-28 00:53:28 +0000

    activity_6.py

---
+++ activity_6.py
@@ -0,0 +1,57 @@
+#!/bin/python
+
+#PROBLEM 1
+# This is a single-line comment
+
+'''
+This is a multi-line comment.
+It spans multiple lines.
+'''
+
+# This is a single-line comment with code:
+print("Hello, World!")  # This is a comment after a statement
+
+#PROBLEM 2
+# Input
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+# Concatenate the strings
+result = string1 + string2
+
+# Output
+print("Concatenated string:", result)
+
+#PROBLEM 3
+# Input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum and product
+sum_result = num1 + num2
+product_result = num1 * num2
+
+# Output
+print("Sum:", sum_result)
+print("Product:", product_result)
+
+#pROBLEM 4
+# Input
+num = float(input("Enter a number: "))
+
+# Calculate the square
+square = num ** 2
+
+# Output
+print("Square:", square)
+
+#PROBLEM 5
+# Input
+num1 = int(input("Enter the dividend: "))
+num2 = int(input("Enter the divisor: "))
+
+# Calculate the remainder
+remainder = num1 % num2
+
+# Output
+print("Remainder:", remainder)
